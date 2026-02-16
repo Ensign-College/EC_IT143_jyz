c689f368e0d4c64c02a07b782fcaf9fd5874f7d3
Jaython123 <zigahj1@gmail.com>
2026-02-16 02:05:19 +0000

    Document table refinement steps in SQL file
    Add instructions for refining the t_hello_world table.

---
+++ W4.2_Hello_World/EC_IT143_W4.2_hello_world_s5_2_jz.sql
@@ -0,0 +1,9 @@
+/*
+Refine table:
+- Add primary key
+- Set Message column to NOT NULL
+You can do this in SSMS GUI:
+1. Right-click table t_hello_world → Design
+2. Add column: HelloWorldID INT IDENTITY(1,1) PRIMARY KEY
+3. Save
+*/
